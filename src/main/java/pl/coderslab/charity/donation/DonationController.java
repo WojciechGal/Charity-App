51d8587a2203aa40a0f2d093fefbaac6e58f3d1e
Wojciech <wojciech.gladalski@gmail.com>
2019-10-11 17:29:06 +0200

    donation controller

--- src/main/java/pl/coderslab/charity/donation/DonationController.java
+++ src/main/java/pl/coderslab/charity/donation/DonationController.java
@@ -1,24 +1,31 @@
-package pl.coderslab.charity.home;
+package pl.coderslab.charity.donation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.coderslab.charity.user.CurrentUser;
 import pl.coderslab.charity.user.User;
 import pl.coderslab.charity.user.UserService;
 
 @Controller
-public class FormController {
+public class DonationController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/form")
-    public String form(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
+    public String formAction(@AuthenticationPrincipal CurrentUser currentUser, Model model) {
         User user = currentUser.getUser();
         model.addAttribute("user", userService.findByUserEmail(user.getEmail()));
         return "form";
+    }
+
+    @PostMapping("/form")
+    public String formAction() {
+
+        return "unknown";
     }
 }
--- src/main/java/pl/coderslab/charity/home/HomeController.java
+++ src/main/java/pl/coderslab/charity/home/HomeController.java
@@ -1,6 +1,5 @@
 package pl.coderslab.charity.home;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.institution.Institution;
 import pl.coderslab.charity.institution.InstitutionService;
 
-import java.awt.print.Book;
 import java.util.List;
-
 
 @Controller
 public class HomeController {
