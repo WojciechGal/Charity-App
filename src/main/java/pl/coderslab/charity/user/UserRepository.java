88587710062baec14a553941d1ab8f2f59811a66
Wojciech <wojciech.gladalski@gmail.com>
2019-10-16 20:45:37 +0200

    updated user repository

--- src/main/java/pl/coderslab/charity/user/UserRepository.java
+++ src/main/java/pl/coderslab/charity/user/UserRepository.java
@@ -1,9 +1,13 @@
 package pl.coderslab.charity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    @Query("select u from User u where u.id = ?1")
+    User findByIdNum(Long id);
 }
