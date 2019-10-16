3fde0ad0aa93ef1cf236b4d890f9d87e73c6421d
Wojciech <wojciech.gladalski@gmail.com>
2019-10-16 20:48:27 +0200

    additional method in user service

--- src/main/java/pl/coderslab/charity/user/UserServiceImpl.java
+++ src/main/java/pl/coderslab/charity/user/UserServiceImpl.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service;
 import pl.coderslab.charity.role.Role;
 import pl.coderslab.charity.role.RoleRepository;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -34,5 +33,63 @@ public class UserServiceImpl implements UserService {
         Role userRole = roleRepository.findByName("ROLE_USER");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         userRepository.save(user);
+    }
+
+    ////////////////////////////////////////////////////////////////
+    public void saveAdmin(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(1);
+        Role userRole = roleRepository.findByName("ROLE_ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        userRepository.save(user);
+    }
+    ///////////////////////////////////////////////////////////////
+
+    public List<User> findUsersWhoAreAdmins() {
+        List<User> all = userRepository.findAll();
+        List<User> admins = new ArrayList<>();
+
+        all.forEach(u -> {
+            int[] counter = {0};
+            Set<Role> roles = u.getRoles();
+            roles.forEach(r -> {
+                if ("ROLE_ADMIN".equals(r.getName())) {
+                    counter[0]++;
+                }
+            });
+            if (counter[0] == 1) {
+                admins.add(u);
+            }
+        });
+        return admins;
+    }
+
+    public List<User> findUsersWhoAreUsers() {
+        List<User> all = userRepository.findAll();
+        List<User> users = new ArrayList<>();
+
+        all.forEach(u -> {
+            int[] counter = {0};
+            Set<Role> roles = u.getRoles();
+            roles.forEach(r -> {
+                if ("ROLE_USER".equals(r.getName())) {
+                    counter[0]++;
+                }
+            });
+            if (counter[0] == 1) {
+                users.add(u);
+            }
+        });
+        return users;
+    }
+
+    public User findUserById(Long id) {
+        User user = userRepository.findByIdNum(id);
+        return user;
+    }
+
+    public void deleteUserById(Long id) {
+
+        userRepository.deleteById(id);
     }
 }
