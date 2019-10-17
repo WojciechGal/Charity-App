7df81503f7f3ed99525434233a8501c69472bdde
Wojciech <wojciech.gladalski@gmail.com>
2019-10-17 15:08:55 +0200

    added new handful and interesting methods in spring data user details service

--- src/main/java/pl/coderslab/charity/user/SpringDataUserDetailsService.java
+++ src/main/java/pl/coderslab/charity/user/SpringDataUserDetailsService.java
@@ -6,7 +6,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.coderslab.charity.exceptions.UserBlockedException;
 
+import javax.security.auth.login.LoginException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,6 +20,15 @@ public class SpringDataUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) {
         User user = userService.findByUserEmail(username);
+
+        ////////////////////sprawdzenie czy user nie jest zablokowany/////////////////////
+
+        if (user.getEnabled() == 0) {
+            throw new UserBlockedException();
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////
+
         if (user == null) {throw new UsernameNotFoundException(username); }
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         user.getRoles().forEach(r ->
