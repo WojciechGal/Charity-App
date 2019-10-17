8e90d5bcdd5c45b919ad09d8c61bfa5e827b0f63
Wojciech <wojciech.gladalski@gmail.com>
2019-10-17 15:07:52 +0200

    added one redirection in webapp

--- src/main/java/pl/coderslab/charity/config/WebAppConfig.java
+++ src/main/java/pl/coderslab/charity/config/WebAppConfig.java
@@ -10,5 +10,6 @@ public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/403").setViewName("authentication/403");
+        registry.addViewController("/admin/deleteMyself").setViewName("admin-sb-2/deleteMyself");
     }
 }
