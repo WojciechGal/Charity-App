5aa2f30615e536b3ccc9b2d2899d8677c0643329
Wojciech <wojciech.gladalski@gmail.com>
2019-10-21 14:17:46 +0200

    updated method in searching donations quantity

--- src/main/java/pl/coderslab/charity/donation/DonationRepository.java
+++ src/main/java/pl/coderslab/charity/donation/DonationRepository.java
@@ -3,10 +3,15 @@ package pl.coderslab.charity.donation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.user.User;
+
+import java.util.List;
 
 @Repository
 public interface DonationRepository extends JpaRepository<Donation, Long> {
 
-    @Query("select sum(quantity) from Donation d")
-    Long countAllQuantities();
+    @Query("select sum(quantity) from Donation d where d.status = 1")
+    Long countAllQuantitiesSend();
+
+    List<Donation> findAllByUser(User user);
 }
