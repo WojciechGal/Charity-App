a9bf492ebf21db961009a3925983bcba8ca554d8
Wojciech <wojciech.gladalski@gmail.com>
2019-10-23 11:02:59 +0200

    updated pass validation

--- src/main/java/pl/coderslab/charity/admin/AdminAdminsController.java
+++ src/main/java/pl/coderslab/charity/admin/AdminAdminsController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,11 +43,26 @@ public class AdminAdminsController {
     }
 
     @PostMapping("/addAdmin")
-    public String addAdmin(@Valid User user, BindingResult result) {
+    public String addAdmin(@Valid User user, BindingResult result, Model model) {
+
+        if (!user.getPass1().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
+            FieldError err = new FieldError("user","pass1","Hasło musi zawierać co najmniej jedną wielką literę, jedną małą literę, cyfrę oraz znak specjalny");
+            result.addError(err);
+        }
+
+        if(!user.getPass1().equals(user.getPass2()) || "".equals(user.getPass2())){
+            FieldError err = new FieldError("user","pass2","Hasło musi zostać poprawnie powtórzone");
+            result.addError(err);
+        }
 
         if (result.hasErrors()) {
+            user.setPass1("");
+            user.setPass2("");
+            model.addAttribute("user", user);
             return "admin-sb-2/addAdmin";
         }
+
+        user.setPassword(user.getPass1());
 
         userService.saveAdmin(user);
         return "redirect:/admin/admins";
@@ -62,11 +78,20 @@ public class AdminAdminsController {
     }
 
     @PostMapping("/updateAdmin/{id}")
-    public String updateAdmin(@Valid User user, BindingResult result) {
+    public String updateAdmin(@Valid User user, BindingResult result, Model model) {
+
+        if (!user.getPass1().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
+            FieldError err = new FieldError("user","pass1","Hasło musi zawierać co najmniej jedną wielką literę, jedną małą literę, cyfrę oraz znak specjalny");
+            result.addError(err);
+        }
 
         if (result.hasErrors()) {
+            user.setPass1("");
+            model.addAttribute("user", user);
             return "admin-sb-2/updateAdmin";
         }
+
+        user.setPassword(user.getPass1());
 
         userService.saveAdmin(user);
         return "redirect:/admin/admins";
